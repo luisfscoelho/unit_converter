e5302ebe1bd4162b039904a66125b5bf1c68606c
Luis Coelho <luis.fs.coelho@pm.me>
2023-01-02 20:16:09 -0300

    base html structure

--- lib/unit_converter_web/live/page_home.ex
+++ lib/unit_converter_web/live/page_home.ex
@@ -5,7 +5,32 @@ defmodule UnitConverterWeb.Live.PageHome do
     ~H"""
       <div>
         <h1>Unit Converter</h1>
-        <p>Uniti converter</p>
+
+        <main>
+          <select name="unit type" id="unit_type">
+            <option value="area">area</option>
+            <option value="length">Length</option>
+            <option value="temperature">Temperature</option>
+            <option value="time">Time</option>
+            <option value="volume">Volume</option>
+            <option value="weight">Weight</option>
+          </select>
+
+          <div style="display: flex">
+            <div>
+              <label for="from">From</label>
+              <select name="from_unit" id="from_unit">
+              </select>
+              <input type="number" id="from_value" name="from_value">
+            </div>
+            <div>
+              <label for="to">To</label>
+              <select name="to_unit" id="to_unit">
+              </select>
+              <input type="number" id="to_value" name="to_value">
+            </div>
+          </div>
+        </main>
       </div>
     """
   end
